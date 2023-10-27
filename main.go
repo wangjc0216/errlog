ff0404a79cb25672a528073caa534bdf1b91ae59
wangjc <wangjc0216@outlook.com>
2023-10-27 15:02:27 +0800

    v2 release

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func init() {
-	fmt.Println("init v1.1")
+	fmt.Println("init v2")
 }
 
 func Errorf(ctx context.Context, a ...any) {
-	fmt.Printf("errlog v1.1:%v", a)
+	fmt.Printf("errlog v2:%v", a)
 }
