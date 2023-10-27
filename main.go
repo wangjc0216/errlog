29ddfee1717c5e4ed00d7b977cd5bba6154a985f
wangjc <wangjc0216@outlook.com>
2023-10-27 15:52:49 +0800

    update tag v2.1

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func init() {
-	fmt.Println("init v2")
+	fmt.Println("init v2.1")
 }
 
 func Errorf(ctx context.Context, a ...any) {
-	fmt.Printf("errlog v2:%v", a)
+	fmt.Printf("errlog v2.1:%v", a)
 }
